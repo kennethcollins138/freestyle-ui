fb7a705bed39157d9090efa463870e7370ee7dfb
kennethcollins138 <kennethcollinswork@gmail.com>
2024-08-05 21:46:47 -0700

    Page Props set up

--- src/types/page.ts
+++ src/types/page.ts
@@ -4,25 +4,30 @@ import type { Schema } from '../api/Schema.js';
 import type { AppController } from '../api/AppController.js';
 
 export type Route = 
-| 'home'
-| 'admin'
-| 'admin:configure'
-| 'welcome';
+  | 'home'
+  | 'admin'
+  | 'admin:configure'
+  | 'welcome';
 
 export type RouteParams = Record<string, string>;
 
 export type PageProps = {
-context: Devvit.Context;
-route: Route;
-navigate: (route: Route, params?: RouteParams) => void;
-params: RouteParams;
+  context: Devvit.Context;
+  route: Route;
+  navigate: (route: Route, params?: RouteParams) => void;
+  params: RouteParams;
 
-currentUserUsername: string;
-isOwner: boolean;
-pinPost: z.infer<(typeof Schema)['AppInstance']>;
-pinPostMethods: {
-    updatePinPost: PinPost['updatePinPost'];
-    updatePinPostElement: PinPost['updatePinPostElement'];
-    clonePost: PinPost['clonePost'];
+  currentUserUsername: string;
+  isOwner: boolean;
+  appPost: z.infer<(typeof Schema)['appInstance']>;
+  postMethods: {
+    updateAppPost: AppController['updateAppInstance'];
+    updateAppElement: AppController['editElement'];
+    addElement: AppController['addChild'];
+    clonePost: AppController['clonePost'];
+    deleteNode: AppController['deleteNode'];
+    readWholePage: AppController['readWholePage'];
+    readImmediateChildren: AppController['readImmediateChildren'];
+    updateChildrenOrder: AppController['updateChildrenOrder'];
+  };
 };
-}
